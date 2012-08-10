a8b93fb384b7ca4f612a62f72432477c298e5b31
amari3 <ryo2.amari3@gmail.com>
2012-08-10 22:58:22 +0900

    initial commit

---
+++ lib/WebService/Microsoft/Translator.pm
@@ -0,0 +1,32 @@
+package WebService::Microsoft::Translator;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Microsoft::Translator -
+
+=head1 SYNOPSIS
+
+  use WebService::Microsoft::Translator;
+
+=head1 DESCRIPTION
+
+WebService::Microsoft::Translator is
+
+=head1 AUTHOR
+
+Ryoji Tanida E<lt>ryo2.amari3@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
